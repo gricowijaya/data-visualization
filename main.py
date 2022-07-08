211465159a85afadd7a6de5d9decec419bffa8cb
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-07-08 15:31:57 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from multiprocessing import Process
 import src.functions as func
 import src.api as url
 import sys
 
 
-def main():
-    func.write_woman_fashions_data(url.womanFashions)
-    pass
+# def main():
+#     # func.write_woman_fashions_data(url.womanFashions)
+#     # func.write_man_fashions_data(url.manFashions)
+#     func.read_woman_fashions_data()
+#     func.read_man_fashions_data()
+#     pass
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    process1 = Process(target=func.read_man_fashions_data())
+    process1.start()
+    process2 = Process(target=func.read_woman_fashions_data())
+    process2.start()
+    # sys.exit(main())
