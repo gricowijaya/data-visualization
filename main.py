e9092903a0245495f03513b64f991da3568a3709
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-07-08 16:07:24 +0800

    update main function

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ import src.api as url
 import sys
 
 
-# def main():
-#     # func.write_woman_fashions_data(url.womanFashions)
-#     # func.write_man_fashions_data(url.manFashions)
-#     func.read_woman_fashions_data()
-#     func.read_man_fashions_data()
-#     pass
+def main():
+    # func.write_woman_fashions_data(url.womanFashions)
+    # func.write_man_fashions_data(url.manFashions)
+    func.read_woman_fashions_data()
+    func.read_man_fashions_data()
+    # func.create_table()
+    pass
 
 
 if __name__ == '__main__':
-    process1 = Process(target=func.read_man_fashions_data())
-    process1.start()
-    process2 = Process(target=func.read_woman_fashions_data())
-    process2.start()
-    # sys.exit(main())
+    sys.exit(main())
