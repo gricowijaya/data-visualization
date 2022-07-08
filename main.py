b182daff8852c730f8b8c08825c703175c145f12
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-07-08 11:25:01 +0800

    main function file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import src.functions as func
+import src.api as url
+import sys
 
+
+def main():
+    func.write_woman_fashions_data(url.womanFashions)
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
