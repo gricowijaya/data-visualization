ef668bfa5b3b70482eb824ad2ef678c3d51574c6
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-07-08 16:07:18 +0800

    add function to convert json into csv

--- src/functions.py
+++ src/functions.py
@@ -1,14 +1,15 @@
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
 import json
 import requests as req
+from markdown import markdown
 
 
 def write_woman_fashions_data(url):
     womanFashions = []
     res = req.get(url)
     data = res.json()
-    file = open(f"./resource/womanFashions.json", "w")
+    file = open(f"./resource/json/womanFashions.json", "w")
     file.write(json.dumps(data))
     file.close
     # pass
@@ -19,16 +20,28 @@ def read_woman_fashions_data():
     id = []
     price = []
     suffix = " Item Has Price below Rp 4000 with the amount of Rp "
-    file = open("./resource/womanFashions.json", "r")
-    data = json.load(file)
-    for item in data:
-        if item['price'] > 4000 and item['price'] < 10000:
-            # add the item name and convert the item price into string
-            # print(item['title'] + suffix + str(item['price']))
-            id.append(item['id'])
-            price.append(item['price'])
-    create_graph(plot_title, id, price)
-    # pass
+    # file = open("./resource/womanFashions.json", "r")
+    # data = json.load(file)
+
+    with open("./resource/json/womanFashions.json", encoding='utf8') as inputFile:
+        data = pd.read_json(inputFile)
+
+    try:
+        data.to_csv('./resource/csv/womanFashions.csv',
+                    encoding='utf8', index=False)
+    except:
+        print('Data is not saved')
+    else:
+        print('CSV data is saved in ./resource/csv/womanFashions.csv')
+
+    # for item in data:
+    #     if item['price'] > 4000 and item['price'] < 10000:
+    #         # add the item name and convert the item price into string
+    #         # print(item['title'] + suffix + str(item['price']))
+    #         id.append(item['id'])
+    #         price.append(item['price'])
+    # create_graph(plot_title, id, price)
+    pass
 
 
 def write_man_fashions_data(url):
@@ -46,16 +59,23 @@ def read_man_fashions_data():
     id = []
     price = []
     suffix = " Item Has Price below Rp 4000 with the amount of Rp "
-    file = open("./resource/manFashions.json", "r")
-    data = json.load(file)
-    for item in data:
-        if item['price'] > 0 and item['price'] < 10000:
-            # add the item name and convert the item price into string
-            # print(item['title'] + suffix + str(item['price']))
-            id.append(item['id'])
-            price.append(item['price'])
-    create_graph(plot_title, id, price)
-    # pass
+    with open("./resource/json/manFashions.json", encoding='utf8') as inputFile:
+        data = pd.read_json(inputFile)
+
+    try:
+        data.to_csv('./resource/csv/manFashions.csv', encoding='utf8', index=False)
+    except:
+        print('Data is not saved')
+    else:
+        print('CSV data is saved in ./resource/csv/manFashions.csv')
+    # for item in data:
+    #     if item['price'] > 0 and item['price'] < 10000:
+    #         # add the item name and convert the item price into string
+    #         print(item['title'] + suffix + str(item['price']))
+    #         id.append(item['id'])
+    #         price.append(item['price'])
+    # create_graph(plot_title, id, price)
+    pass
 
 
 def create_graph(plot_title, title, price):
@@ -68,4 +88,12 @@ def create_graph(plot_title, title, price):
     plt.show()
     # pass
 
+
 def create_table():
+    table = f"""
+    |ID Product | Price |
+    |-----------|-------|
+    |-----------|-------|
+    """
+    html = markdown(table, extensions=["tables"])
+    print(html)
