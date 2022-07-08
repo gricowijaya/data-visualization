6aaaf3e2279c6db76c49804693581a06b5ce88dd
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-07-08 18:44:25 +0800

    refactor, minimize the variable and put the filename

--- src/functions.py
+++ src/functions.py
@@ -1,58 +1,75 @@
+from markdown import markdown
+from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
 import requests as req
 import numpy as np
 import csv
-from markdown import markdown
+import sys
 
 
 def get_data_and_write(url):
+
     # print(filename[0:16])  # only get the ./resource/json/data.json/strings
     # print(womanFashions[29:29])  # only get the http://ml.cakeplabs.com:1891/
     # only get the path-name for the filename /woman-fashions
     # print(womanFashions[29:50])
 
-    prefix_json_filename = f"./resource/json/"
-    prefix_csv_filename = f"./resource/csv/"
-    filename = f'{url[29:50]}'
+    # only the get path name since it started from the 29th characters
+    json_filename = f"./resource/json/{url[29:50]}.json"
+    csv_filename = f"./resource/csv/{url[29:50]}.csv"
+    json_path = Path(json_filename)
+    csv_path = Path(csv_filename)
 
+    # check If the file exists or not
+    if (json_path.is_file() or csv_path.is_file()) :
+        pass
+    
     try:
         # get the data from the API
         res = req.get(url)
         data = res.json()
 
         # write to json
-        file = open(f'{prefix_json_filename}{filename}.json', "w")
+        file = open(json_filename, "w")
         file.write(json.dumps(data))
         file.close
 
         # write to csv
-        with open(f'{prefix_json_filename}{filename}.json', encoding='utf8') as inputFile:
+        with open(json_filename, encoding='utf8') as inputFile:
             data = pd.read_json(inputFile)
-        data.to_csv(f'{prefix_csv_filename}{filename}.csv',
+        data.to_csv(csv_filename,
                     encoding='utf8', index=False)
     except:
         print('Data is not saved')
+        print(sys.exc_info())
     else:
-        print(f'CSV data is saved in {prefix_csv_filename}{filename}.csv')
-        print(f'JSON data is saved in {prefix_csv_filename}{filename}.json')
+        print(f'CSV data is saved in {csv_filename}')
+        print(f'JSON data is saved in {json_filename}')
+
+
+def get_csv_data(filename):
+    data = pd.read_csv(filename, index_col='id')
+    return data[0:5]
+    pass
 
 
 def filter_data_by_price(url, start_value, end_value):
-    prefix_json_filename = f"./resource/json/"
-    prefix_csv_filename = f"./resource/csv/"
-    filename = f'{url[29:50]}'
-    data = pd.read_csv(f"{prefix_csv_filename}{filename}.csv", index_col='id')
-    print(data[0:5])
+    id = []
+    price = []
+
+    json_filename = f"./resource/json/{url[29:50]}.json"
+    csv_filename = f"./resource/csv/{url[29:50]}.csv"
+
+    data = get_csv_data(csv_filename)
+    # print(data[0:])
 
     # for item in data:
-    #     if item['price'] > start_value and item['price'] < end_value:
-    #         # add the item name and convert the item price into string
-    #         # print(item['title'] + suffix + str(item['price']))
+    #     if int(item[0:]['price']) > f'{start_value}' and int(item[0:]['price']) <= end_value:
     #         id.append(item['id'])
     #         price.append(item['price'])
-    # create_graph(plot_title, id, price)
+    create_graph(csv_filename.capitalize(), id, price)
     pass
 
 
@@ -64,7 +81,7 @@ def create_graph(plot_title, title, price):
     plt.xlabel("ID Product")
     plt.ylabel("Price")
     plt.show()
-    # pass
+    pass
 
 
 def create_table():
@@ -75,20 +92,4 @@ def create_table():
     """
     html = markdown(table, extensions=["tables"])
     print(html)
-
-
-def read_csv_data(filename):
-    # with open('./resource/csv/manFashions.csv') as data:
-    #     csv_reader = csv.reader(data, delimiter=',')
-    #     line_count = 0
-    #     for row in csv_reader:
-    #         if line_count == 0:
-    #             print(f'Column name for this are {", ".join(row)}')
-    #             line_count += 1
-    #         else:
-    #             print(f'Column name for this are {", ".join(row)}')
-    #             line_count += 1
-    #     print(f'Processed {line_count} lines.')
-
-    data = pd.read_csv(f"./resource/csv/{filename}.csv", index_col='id')
-    print(data[0:5])
+    pass
