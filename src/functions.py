fcca9cf3ae3cf8e3439dfac9df7a4a95092b456e
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-07-21 18:14:47 +0800

    add function and graph

--- src/functions.py
+++ src/functions.py
@@ -1,11 +1,13 @@
 from markdown import markdown
 from pathlib import Path
+from sklearn.cluster import KMeans  # For Clustering
+from sklearn.preprocessing import MinMaxScaler  # For Preprocessing
+from sklearn.datasets import make_blobs  # For Preprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
 import requests as req
 import numpy as np
-import csv
 import sys
 
 
@@ -23,9 +25,9 @@ def get_data_and_write(url):
     csv_path = Path(csv_filename)
 
     # check If the file exists or not
-    if (json_path.is_file() or csv_path.is_file()) :
+    if (json_path.is_file() or csv_path.is_file()):
         pass
-    
+
     try:
         # get the data from the API
         res = req.get(url)
@@ -55,41 +57,60 @@ def get_csv_data(filename):
     pass
 
 
-def filter_data_by_price(url, start_value, end_value):
-    id = []
-    price = []
+def create_graph(url, start_value, end_value):
 
-    json_filename = f"./resource/json/{url[29:50]}.json"
+    # json_filename = f"./resource/json/{url[29:50]}.json"
+    # csv_filename = f"./resource/csv/{url[29:50]}.csv"
+
+    # # data = get_csv_data(csv_filename)
+    # data = pd.read_csv(csv_filename)
+
+    # km = KMeans(n_clusters=5)
+
+    # y_predicted = km.fit_predict(data[['id', 'price']])
+    # data['cluster'] = y_predicted
+
+    # data1 = data[data.cluster == 0]
+    # data2 = data[data.cluster == 1]
+    # data3 = data[data.cluster == 2]
+    # data4 = data[data.cluster == 3]
+    # data5 = data[data.cluster == 4]
+
+    # plt.scatter(data1.id,  data1['price'], color='green')
+    # plt.scatter(data2.id,  data2['price'], color='red')
+    # plt.scatter(data3.id,  data3['price'], color='black')
+    # plt.scatter(data4.id,  data4['price'], color='blue')
+    # plt.scatter(data5.id,  data5['price'], color='yellow')
+    # plt.scatter(km.cluster_centers_[:, 0], km.cluster_centers_[
+    #             :, 1], color='purple', marker='*', label='centroid')
+
+    # plt.xlabel('id')
+    # plt.ylabel('price')
+    # plt.show()
+
+    # scaler = MinMaxScaler()
+
+    # data['price'] = scaler.transform(data['price'])
+    # scaler.fit(data.id)
+    # data.id = scaler.transform(data.id)
+    # print(data)
+
+    # y_predicted = km.fit_predict(data[['id', 'price']])
+    # data['cluster'] = y_predicted
+    # k_rng = range(1, 10)
+    # sse = []
+    # # for k in k_rng:
+    # #     km = KMeans(n_clusters=k)
+    # #     km.fit(data[['price']])
+    # #     sse.append(km.intertia_)
+    # # plt.plot(range(1, 11), sse)
+    # # plt.show()
+
+    pass
+
+
+def elbow_range(url):
     csv_filename = f"./resource/csv/{url[29:50]}.csv"
+    data = pd.read_csv(csv_filename)
 
-    data = get_csv_data(csv_filename)
-    # print(data[0:])
-
-    # for item in data:
-    #     if int(item[0:]['price']) > f'{start_value}' and int(item[0:]['price']) <= end_value:
-    #         id.append(item['id'])
-    #         price.append(item['price'])
-    create_graph(csv_filename.capitalize(), id, price)
-    pass
-
-
-def create_graph(plot_title, title, price):
-    x = title
-    y = price
-    plt.plot(x, y)
-    plt.title(plot_title)
-    plt.xlabel("ID Product")
-    plt.ylabel("Price")
-    plt.show()
-    pass
-
-
-def create_table():
-    table = f"""
-    |ID Product | Price |
-    |-----------|-------|
-    |-----------|-------|
-    """
-    html = markdown(table, extensions=["tables"])
-    print(html)
-    pass
+    km = KMeans(n_clusters=5)
